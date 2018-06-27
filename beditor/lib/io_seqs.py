75f347e47f2f6801115e31a70ab322c0de706bd1
rraadd88 <rraadd_8@hotmail.com>
2018-06-27 14:26:18 -0400

    common io stuff separated

---
+++ beditor/lib/io_seqs.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def fa2df(alignedfastap,ids2cols=False):
+    dtmp=pd.read_csv(alignedfastap,names=["c"])
+    dtmp=dtmp.iloc[::2].reset_index(drop=True).join(dtmp.iloc[1::2].reset_index(drop=True),rsuffix='r')
+    dtmp.columns=['id','sequence']
+    dtmp=dtmp.set_index('id')
+    dtmp.index=[i[1:] for i in dtmp.index]
+    if ids2cols:
+        for i in dtmp.index:
+            seqid,contig,strand,start,end=i.split('|')
+            dtmp.loc[i,'seqid']=seqid
+            dtmp.loc[i,'contig']=contig
+            dtmp.loc[i,'strand']=strand
+            dtmp.loc[i,'start']=start
+            dtmp.loc[i,'end']=end
+    return dtmp
---
+++ beditor/lib/io_sys.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def runbashcmd(cmd):
+    from beditor.lib.global_vars import dirs2ps 
+    cmd = cmd.replace("$BIN", dirs2ps['binDir'])
+    cmd = cmd.replace("$PYTHON", dirs2ps['pyp'])
+    cmd = cmd.replace("$SCRIPT", dirs2ps['scriptDir'])
+#     print(cmd)
+    err=subprocess.call(cmd,shell=True)
+    if err!=0:
+        print('bash command error: {}\n{}\n'.format(err,cmd))
+        sys.exit(1)
