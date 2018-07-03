e6a23da68024301e0462dbec02a9918012d4c82b
rraadd88 <rraadd_8@hotmail.com>
2018-07-03 17:03:16 -0400

    done: dannotagg

--- beditor/lib/get_specificity.py
+++ beditor/lib/get_specificity.py
@@ -18,6 +18,8 @@ import pysam
 import numpy as np
 from glob import glob
 
+from tqdm import trange
+
 from beditor.lib.io_sys import runbashcmd
 from beditor.lib.io_seqs import fa2df 
 from beditor.lib.io_dfs import set_index,del_Unnamed 
@@ -52,20 +54,22 @@ def align(s1,s2,test=False):
         break
     return alignsymb,score
 def gffatributes2ids(s):
-    d=dict([i.split('=') for i in s.split(';')])
-    if 'Parent' in d:
-        d[d['Parent'].split(':')[0]+'_id']=d['Parent'].split(':')[1]
     Name,gene_id,transcript_id,protein_id,exon_id=np.nan,np.nan,np.nan,np.nan,np.nan
-    if 'Name' in d:    
-        Name=d['Name']
-    if 'gene_id' in d:    
-        gene_id=d['gene_id']
-    if 'transcript_id' in d:    
-        transcript_id=d['transcript_id']
-    if 'protein_id' in d:    
-        protein_id=d['protein_id']
-    if 'exon_id' in d:    
-        exon_id=d['exon_id']
+    if '=' in s:
+        d=dict([i.split('=') for i in s.split(';')])
+        if 'Parent' in d:
+            d[d['Parent'].split(':')[0]+'_id']=d['Parent'].split(':')[1]
+        Name,gene_id,transcript_id,protein_id,exon_id=np.nan,np.nan,np.nan,np.nan,np.nan
+        if 'Name' in d:    
+            Name=d['Name']
+        if 'gene_id' in d:    
+            gene_id=d['gene_id']
+        if 'transcript_id' in d:    
+            transcript_id=d['transcript_id']
+        if 'protein_id' in d:    
+            protein_id=d['protein_id']
+        if 'exon_id' in d:    
+            exon_id=d['exon_id']
     return Name,gene_id,transcript_id,protein_id,exon_id
 #--------------------------------------------
 from beditor.lib.io_dfs import lambda2cols
@@ -225,6 +229,7 @@ def dguides2offtargets(cfg):
 
         dalignedfasta=fa2df(alignedfastap)
         dalignedfasta.columns=['aligned sequence']
+        dalignedfasta.index=[i.split('(')[0] for i in dalignedfasta.index] # for bedtools 2.27, the fasta header now has hanging (+) or (-)
         dalignedfasta.to_csv(dalignedfastap,sep='\t')
     else:
         dalignedfasta=pd.read_csv(dalignedfastap,sep='\t')        
@@ -262,27 +267,66 @@ def dguides2offtargets(cfg):
         dalignbed=pd.read_csv(dalignbedstatsp,sep='\t',low_memory=False)
         dalignbed=del_Unnamed(dalignbed)
             
-    dalignbedannotp='{}/dalignbedannot.tsv'.format(datatmpd)  
-    logging.info(basename(dalignbedannotp))
-    if not exists(dalignbedannotp) or cfg['force']:
+    daannotp='{}/dannot.tsv'.format(datatmpd)  
+    logging.info(basename(daannotp))
+    if not exists(daannotp) or cfg['force']:
         dannots=pd.read_csv('{}/annotations.bed'.format(datatmpd),sep='\t',
-                   names=bed_colns+gff_colns,
+                   names=bed_colns+[c+' annotation' if c in set(bed_colns).intersection(gff_colns) else c for c in gff_colns ],
                            low_memory=False)
         dannots=del_Unnamed(dannots)
 
-        dcombo=set_index(dalignbed,'id').join(set_index(dannots,'id'),
-                                              rsuffix='.2')
-        dcombo=dcombo.drop_duplicates(subset=['type','guide: id','start.1','end.1'])
-        #separate ids from attribute columns
-#         dcombo=lambda2cols(dcombo,lambdaf=gffatributes2ids,
-#                     in_coln='attributes',
-#                 to_colns=['gene name','gene id','transcript id','protein id','exon id'])
+        dannots=dannots.set_index('id')
+        dannots['annotations count']=1
+        # separate ids from attribute columns
+        dannots=lambda2cols(dannots,lambdaf=gffatributes2ids,
+                    in_coln='attributes',
+                to_colns=['gene name','gene id','transcript id','protein id','exon id'])
+
+        dannots['annotation coordinate']=dannots.apply(lambda x: '{}:{}-{}({})'.format(x['chromosome annotation'],x['start annotation'],x['end annotation'], x['strand annotation']),axis=1)
+        dannots.to_csv(daannotp,sep='\t')
+    else:
+        dannots=pd.read_csv(daannotp,sep='\t',low_memory=False)
+        dannots=del_Unnamed(dannots)
+
+    dannotsaggp='{}/dannotsagg.tsv'.format(datatmpd)  
+    logging.info(basename(dannotsaggp))
+    if not exists(dannotsaggp) or cfg['force']:
+        dannotsagg=pd.DataFrame(dannots.groupby('id')['annotations count'].agg('sum'))-1
+        dannotsagg.loc[dannotsagg['annotations count']==0,'region']='intergenic'
+        dannotsagg.loc[dannotsagg['annotations count']!=0,'region']='genic'
+
+        dannots=dannots.reset_index()
+        alignids=dannots['id'].unique()[:15]
+        
+        for alignidi in trange(len(alignids)):
+            alignid=alignids[alignidi]
+        #     if dannots.loc[i,'type'].value_counts().sum()==1:
+            dannoti=dannots.loc[dannots['id']==alignid,:]
+            if len(dannoti.shape)==1:
+                dannoti=pd.DataFrame(dannoti).T
+            dannoti=dannoti.loc[dannoti['type']!='chromosome',:].drop_duplicates(subset=['start annotation','end annotation'])
+#             if cfg['test']:
+# #                 print(alignid)
+#                 print(dannoti.shape)
+            for col in ['type','gene name','gene id','transcript id','protein id','exon id','annotation coordinate']:    
+        #         print(";".join(dannoti[col].fillna('nan').tolist()))
+                dannotsagg.loc[alignid,col+'s']=";".join(dannoti[col].fillna('nan').tolist())
+        dannotsagg.to_csv(dannotsaggp,sep='\t')
+    else:
+        dannotsagg=pd.read_csv(dannotsaggp,sep='\t',low_memory=False)
+        dannotsagg=del_Unnamed(dannotsagg)
+        
+    dalignbedannotp='{}/dalignbedannot.tsv'.format(datatmpd)  
+    logging.info(basename(dalignbedannotp))
+    if not exists(dalignbedannotp) or cfg['force']:
+        dcombo=set_index(dalignbed,'id').join(set_index(dannotsagg,'id'),
+                                              rsuffix=' annotation')
         dcombo.to_csv(dalignbedannotp,sep='\t')
     else:
         dcombo=pd.read_csv(dalignbedannotp,sep='\t',low_memory=False)
         dcombo=del_Unnamed(dcombo)
-    print('{}/dofftargets.tsv'.format(cfg['datad']))
-    dcombo.to_csv('{}/dofftargets.tsv'.format(cfg['datad']),sep='\t')
+    # print('{}/dofftargets.tsv'.format(cfg['datad']))
+    # dcombo.to_csv('{}/dofftargets.tsv'.format(cfg['datad']),sep='\t')
 
 
 # if __name__ == '__main__':
