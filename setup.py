9060dc468142b7d7f9ec45929e5977321eb36466
rraadd88 <rraadd_8@hotmail.com>
2018-07-28 13:41:48 +0530

    version bump

--- setup.py
+++ setup.py
@@ -21,22 +21,20 @@ try:
     from setuptools import setup, find_packages, Extension 
 except ImportError:
     from distutils.core import setup, find_packages, Extension
-
-
 if (sys.version_info[0], sys.version_info[1],sys.version_info[2]) != (3, 6 ,5):
     raise RuntimeError('Python 3.6.5 required ')
  
-# main setup command
+# main setup
 setup(
 name='beditor',
 author='Rohan Dandage',
-author_email='rraadd_8@hotmail.com',
-version='0.0.1',
-# url='https://github.com/rraadd88/lrt',
-# download_url='https://github.com/rraadd88/lrt/archive/master.zip',
-# description='toolbox for rats in the labs',
-# long_description='https://github.com/rraadd88/lrt/README.md',
-# keywords=['lab','rat','tools'],
+author_email='rraadd8888@gmail.com, rohan.dandage.1@ulaval.ca',
+version='1.0.0',
+url='https://github.com/rraadd88/beditor',
+download_url='https://github.com/rraadd88/beditor/archive/master.zip',
+# description='',
+long_description='https://github.com/rraadd88/beditor/README.md',
+# keywords=['','',''],
 license='General Public License v. 3',
 install_requires=['biopython==1.71',
                   'regex==2018.7.11',
@@ -50,13 +48,9 @@ install_requires=['biopython==1.71',
                     'tqdm==4.23.4',
                     'seaborn==0.8.1',
                     'pyensembl==1.4.0'],
-
-
-
-    
-platforms='Tested on Ubuntu 16.04',
+platforms='Tested on Ubuntu 16.04 64bit',
 packages=find_packages(),
-package_data={'': ['beditor/tmp','beditor/cfg']},
+package_data={'': ['beditor/data']},
 include_package_data=True,
 entry_points={
     'console_scripts': ['beditor = beditor.pipeline:main',],
