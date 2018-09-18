d96d563091095970bd982fcbe44290f5703f9b00
rraadd88 <rraadd_8@hotmail.com>
2018-09-17 23:02:35 -0400

    binary

--- beditor/lib/io_dfs.py
+++ beditor/lib/io_dfs.py
@@ -239,18 +239,21 @@ def filldiagonal(df,cols,filler=None):
         df.loc[r,c]=filler
     return df
 
-def df2submap(df,col,idx,aggfunc='sum',binary=False):
+def df2submap(df,col,idx,aggfunc='sum',binary=False,binaryby='nan'):
     df['#']=1
     dsubmap=df.pivot_table(columns=col,index=idx,values='#',
                        aggfunc=aggfunc)
     if binary:
-        dsubmap=~pd.isnull(dsubmap)
+        if binaryby=='nan':
+            dsubmap=~pd.isnull(dsubmap)
+        else:
+            dsubmap=dsubmap!=binaryby
     return dsubmap
 
 def completesubmap(dsubmap,fmt,
     fmt2vals={'aminoacid':["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y","*"], 
     'aminoacid_3letter':['ALA','ARG','ASN','ASP','CYS','GLN','GLU','GLY','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL'],
-    'codons':["TTT",    "TTC",    "TTA",  "TTG",  "TCT",  "TCC",  "TCA",  "TCG",  "TAT",  "TAC",  "TAA",  "TAG",  "TGT",  "TGC",  "TGA",  "TGG",  "CTT",  "CTC",  "CTA",  "CTG",  "CCT",  "CCC",  "CCA",  "CCG",  "CAT",  "CAC",  "CAA",  "CAG",  "CGT",  "CGC",  "CGA",  "CGG",  "ATT",  "ATC",  "ATA",  "ATG",  "ACT",  "ACC",  "ACA",  "ACG",  "AAT",  "AAC",  "AAA",  "AAG",  "AGT",  "AGC",  "AGA",  "AGG",  "GTT",  "GTC",  "GTA",  "GTG",  "GCT",  "GCC",  "GCA",  "GCG",  "GAT",  "GAC",  "GAA",  "GAG",  "GGT",  "GGC",  "GGA",  "GGG"],
+    'codon':["TTT",    "TTC",    "TTA",  "TTG",  "TCT",  "TCC",  "TCA",  "TCG",  "TAT",  "TAC",  "TAA",  "TAG",  "TGT",  "TGC",  "TGA",  "TGG",  "CTT",  "CTC",  "CTA",  "CTG",  "CCT",  "CCC",  "CCA",  "CCG",  "CAT",  "CAC",  "CAA",  "CAG",  "CGT",  "CGC",  "CGA",  "CGG",  "ATT",  "ATC",  "ATA",  "ATG",  "ACT",  "ACC",  "ACA",  "ACG",  "AAT",  "AAC",  "AAA",  "AAG",  "AGT",  "AGC",  "AGA",  "AGG",  "GTT",  "GTC",  "GTA",  "GTG",  "GCT",  "GCC",  "GCA",  "GCG",  "GAT",  "GAC",  "GAA",  "GAG",  "GGT",  "GGC",  "GGA",  "GGG"],
     'nucleotide': ['A','T','G','C'],}):
     
     vals=fmt2vals[fmt]
