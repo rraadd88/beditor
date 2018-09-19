fa794153c58bb2b0bdf0f03313612b2b25164a2b
rraadd88 <rraadd_8@hotmail.com>
2018-09-19 15:37:48 -0400

    sphinx: added

---
+++ test/test_dataset.py
@@ -0,0 +1,19 @@
+#     import subprocess
+from beditor.lib.io_sys import runbashcmd
+from os.path import exists
+
+def test_species(host='saccharomyces_cerevisiae'):
+    # clone test_beditor 
+    if not exists('test_beditor'):
+        runbashcmd('git clone https://github.com/rraadd88/test_beditor.git',test=True)
+    else:
+        runbashcmd('cd test_beditor;git pull',test=True)
+    com=f'source activate beditor;cd test_beditor;python make_datasets.py --species saccharomyces_cerevisiae --genomerelease 92 --genomeassembly R64-1-1'
+    runbashcmd(com,test=True)
+    
+    cfgp='mutation_format_nucleotide_mutation_mutations_for.yml'
+    com=f"source activate beditor;cd test_beditor/dataset_saccharomyces_cerevisiae;beditor --cfg {cfgp}"
+    runbashcmd(com,test=True)
+
+    
+test_species()
