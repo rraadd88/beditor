34a2019eca7feb65a4222a0dd000901b7c578951
rraadd88 <rraadd_8@hotmail.com>
2018-10-14 20:09:47 -0400

    dataset

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -1,4 +1,3 @@
-#     import subprocess
 from beditor.lib.io_sys import runbashcmd
 from os.path import exists
 
@@ -10,10 +9,5 @@ def test_species(host='saccharomyces_cerevisiae'):
         runbashcmd('cd test_beditor;git pull',test=True)
     com=f'source activate beditor;cd test_beditor;python test_datasets.py'
     runbashcmd(com,test=True)
-    
-#     cfgp='mutation_format_nucleotide_mutation_mutations_for.yml'
-#     com=f"source activate beditor;cd test_beditor/dataset_saccharomyces_cerevisiae;beditor --cfg {cfgp}"
-#     runbashcmd(com,test=True)
-
     
 test_species()
